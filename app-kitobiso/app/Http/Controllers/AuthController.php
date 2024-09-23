d0101f715ac9fc08b62e65756778d4c999e5eca1
FatimahTM <161033481+FatimahTM@users.noreply.github.com>
2024-09-23 19:08:25 +0700

    e

--- app-kitobiso/app/Http/Controllers/AuthController.php
+++ app-kitobiso/app/Http/Controllers/AuthController.php
@@ -2,46 +2,41 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
-use Hash;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
+    function login(Request $request) {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
             'device_name' => 'required',
         ]);
-
+     
         $user = User::where('email', $request->email)->first();
-
-        if (!$user || !Hash::check($request->password, $user->password)) {
+     
+        if (! $user || ! Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['The provided credentials are incorrect.'],
             ]);
         }
-
-        // Jika menggunakan token (misalnya Laravel Passport atau Sanctum)
-        $token = $user->createToken($request->device_name)->plainTextToken;
-
+     
         return response()->json([
-            'message' => 'success',
-            'token' => $token,
+            'token' => $user->createToken($request->device_name)->plainTextToken,
+            'message' => 'Success'
         ]);
     }
 
-    public function register(Request $request)
-    {
+    function register(Request $request) {
         $request->validate([
-            'email' => 'required|email|unique:users,email', 
-            'password' => 'required|min:8'
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
             'name' => 'required',
         ]);
-
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
@@ -49,14 +44,16 @@ class AuthController extends Controller
         $user->save();
 
         return response()->json([
-            'message' => 'success',
+            'message' => 'Success'
         ]);
+    
     }
 
     function logout(Request $request) {
         $request->user()->currentAccessToken()->delete();
         return response()->json([
-            'message' => 'success',
+            'message' => 'Success'
         ]);
     }
+
 }
